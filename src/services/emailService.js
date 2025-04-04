819c84b7f52103ecb6032c2ec0aff3a217be3090
Nuppasorn-Pop <nuppasorn.s@gmail.com>
2025-04-04 19:39:14 +0700

    change domain

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -1,14 +1,11 @@
 export const sendEmail = async (dataFromInput, subject) => {
   try {
-    const response = await fetch(
-      "http://www.chanasoapofficial.com/send-email",
-      {
-        // const response = await fetch("http://localhost:8888/send-email", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ dataFromInput, subject }),
-      }
-    );
+    const response = await fetch("http://www.artandalice.co/send-email", {
+      // const response = await fetch("http://localhost:8888/send-email", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ dataFromInput, subject }),
+    });
 
     return response;
   } catch (error) {
